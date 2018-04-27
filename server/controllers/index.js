52ee4eaef1be225207b19ddbb4f327a88e2f6ed1
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2018-04-26 22:36:43 -0300

    código funfando

---
+++ server/controllers/index.js
@@ -0,0 +1,6 @@
+const todos = require('./todo');
+const todoItems = require('./todoitems');
+
+module.exports = {
+  todos,todoItems,
+};
---
+++ server/routes/index.js
@@ -0,0 +1,16 @@
+const todosController = require('../controllers').todos;
+const todoItemsController = require('../controllers').todoItems;
+
+module.exports = (app) => {
+    app.get('/api', (req, res) => res.status(200).send({
+      message: 'Welcome to the Todos API!',
+    }));
+  
+    app.post('/api/todos', todosController.create);
+    app.get('/api/todos', todosController.list);
+    app.get('/api/todos/all', todosController.list_find_all);
+    app.post('/api/todos/:todoId/items', todoItemsController.create);
+    app.get('/api/todos/:todoId', todosController.retrieve);
+    app.put('/api/todos/:todoId', todosController.update);
+    app.delete('/api/todos/:todoId', todosController.destroy);
+  };
